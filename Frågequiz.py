f95589eccc3de613bc76106dc80e18f29a2bf871
ArloLeo <leo.eveborn@gmail.com>
2025-06-06 11:11:58 +0000

    Pending changes exported from your codespace

---
+++ Frågequiz.py
@@ -0,0 +1,166 @@
+while True:
+    Fråga1 = input("Fråga 1.\n 1, X, 2")
+    if "x" in Fråga1:
+        fråga1 = "Fråga 1: X"
+        break
+    elif "1" in Fråga1:
+        fråga1 = "Fråga 1: 1"
+        break
+    elif "X" in Fråga1:
+        fråga1 = "Fråga 1: X"
+        break
+    elif "2" in Fråga1:
+        fråga1 = "Fråga 1: 2"
+        break
+    else:
+        print("Skriv 1, x eller 2 för att svara.")
+while True:
+    Fråga2 = input("Fråga 2.\n 1, X, 2")
+    if "x" in Fråga2:
+        fråga2 = "Fråga 2: X"
+        break
+    elif "1" in Fråga2:
+        fråga2 = "Fråga 2: 1"
+        break
+    elif "X" in Fråga2:
+        fråga2 = "Fråga 2: X"
+        break
+    elif "2" in Fråga2:
+        fråga2 = "Fråga 2: 2"
+        break
+    else:
+        print("Skriv 1, x eller 2 för att svara.")
+while True:
+    Fråga3 = input("Fråga 3. 1, X, 2")
+    if "x" in Fråga3:
+        fråga3 = "Fråga 3: X"
+        break
+    elif "1" in Fråga3:
+        fråga3 = "Fråga 3: 1"
+        break
+    elif "X" in Fråga3:
+        fråga3 = "Fråga 3: X"
+        break
+    elif "2" in Fråga1:
+        fråga3 = "Fråga 3: 2"
+        break
+    else:
+        print("Skriv 1, x eller 2 för att svara.")
+while True:
+    Fråga4 = input("Fråga 4. 1, X, 2")
+    if "x" in Fråga4:
+        fråga4 = "Fråga 4: X"
+        break
+    elif "1" in Fråga4:
+        fråga4 = "Fråga 4: 1"
+        break
+    elif "X" in Fråga4:
+        fråga4 = "Fråga 4: X"
+        break
+    elif "2" in Fråga4:
+        fråga4 = "Fråga 4: 2"
+        break
+    else:
+        print("Skriv 1, x eller 2 för att svara.")
+while True:
+    Fråga5 = input("Fråga 5. 1, X, 2")
+    if "x" in Fråga5:
+        fråga5 = "Fråga 5: X"
+        break
+    elif "1" in Fråga5:
+        fråga5 = "Fråga 5: 1"
+        break
+    elif "X" in Fråga5:
+        fråga5 = "Fråga 5: X"
+        break
+    elif "2" in Fråga5:
+        fråga5 = "Fråga 5: 2"
+        break
+    else:
+        print("Skriv 1, x eller 2 för att svara.")
+while True:
+    Fråga6 = input("Fråga 6. 1, X, 2")
+    if "x" in Fråga6:
+        fråga6 = "Fråga 6: X"
+        break
+    elif "1" in Fråga6:
+        fråga6 = "Fråga 6: 1"
+        break
+    elif "X" in Fråga6:
+        fråga6 = "Fråga 6: X"
+        break
+    elif "2" in Fråga6:
+        fråga6 = "Fråga 6: 2"
+        break
+    else:
+        print("Skriv 1, x eller 2 för att svara.")
+while True:
+    Fråga7 = input("Fråga 7. 1, X, 2")
+    if "x" in Fråga7:
+        fråga7 = "Fråga 7: X"
+        break
+    elif "1" in Fråga7:
+        fråga7 = "Fråga 7: 1"
+        break
+    elif "X" in Fråga7:
+        fråga7 = "Fråga 7: X"
+        break
+    elif "2" in Fråga7:
+        fråga7 = "Fråga 7: 2"
+        break
+    else:
+        print("Skriv 1, x eller 2 för att svara.")    
+while True:
+    Fråga8 = input("Fråga 8. 1, X, 2")
+    if "x" in Fråga8:
+        fråga8 = "Fråga 8: X"
+        break
+    elif "1" in Fråga8:
+        fråga8 = "Fråga 8: 1"
+        break
+    elif "X" in Fråga8:
+        fråga8 = "Fråga 8: X"
+        break
+    elif "2" in Fråga8:
+        fråga8 = "Fråga 8: 2"
+        break
+    else:
+        print("Skriv 1, x eller 2 för att svara.")
+while True:
+    Fråga9 = input("Fråga 9. 1, X, 2")
+    if "x" in Fråga9:
+        fråga9 = "Fråga 9: X"
+        break
+    elif "1" in Fråga9:
+        fråga9 = "Fråga 9: 1"
+        break
+    elif "X" in Fråga9:
+        fråga9 = "Fråga 9: X"
+        break
+    elif "2" in Fråga9:
+        fråga9 = "Fråga 9: 2"
+        break
+    else:
+        print("Skriv 1, x eller 2 för att svara.")   
+while True:
+    Fråga10 = input("Fråga 10. 1, X, 2")
+    if "x" in Fråga10:
+        fråga10 = "Fråga 10: X"
+        break
+    elif "1" in Fråga10:
+        fråga10 = "Fråga 10: 1"
+        break
+    elif "X" in Fråga10:
+        fråga10 = "Fråga 10: X"
+        break
+    elif "2" in Fråga10:
+        fråga10 = "Fråga 10: 2"
+        break
+    else:
+        print("Skriv 1, x eller 2 för att svara.")   
+    
+    
+    
+    
+print("\n", fråga1, "\n", fråga2, "\n", fråga3, "\n", fråga4, "\n", fråga5, "\n", fråga6, "\n", fråga7, "\n", fråga8, "\n", fråga9, "\n", fråga10)
+
